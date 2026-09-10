fc9f22e2f966d564ff18dcc9f260cc4247acb817
hvalerm <hvalerm@gmail.com>
2026-09-10 01:42:22 -0500

    Agregando registro - mejora2

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
 $host = getenv('DB_HOST');
 $port = getenv('DB_PORT');
-$dbname = getenv('DB_NAME');
+//$dbname = getenv('DB_NAME');
+$dbname = 'opendoors';
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
 
 try {
-    $dsn = "mysql:host=$host;port=$port;dbname=opendoors;charset=utf8mb4";
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
     
     $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
