b3d7319fbef29cd7e162f2bd6688ec2dbf3f732c
hvalerm <hvalerm@gmail.com>
2026-09-09 20:39:37 -0500

    adjuntando archivo Docker

--- login.php
+++ login.php
@@ -52,6 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $error = "No detecta";
                 break;
         }
+        exit;
         
         header("Location: dashboard.php");
         exit;
