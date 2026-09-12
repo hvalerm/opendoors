967b67f392a16f3c71240622a8ff8ad20f4d9d6f
hvalerm <hvalerm@gmail.com>
2026-09-11 19:53:42 -0500

    Nueva version: Mejora en la estructura de archivos Mejora en la modularizacion Mejora en la reutilizacion de codigo Mejora para el modo debug

--- db.php
+++ db.php
@@ -1,25 +1,10 @@
 <?php
-
+session_start();
 $host = getenv('DB_HOST');
 $port = getenv('DB_PORT');
 $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
-
-$debug = 0;
-
-if ($debug == 1) {
-    
-    if (!isset($mi_rol)) {
-        $mi_rol = 0;
-    }
-    
-    $host = 'mysql-25019e99-opendoors.j.aivencloud.com';
-    $port = '13195';
-    $dbname = 'opendoors';
-    $user = 'avnadmin';
-    $pass = 'AVNS_9BjfJpsOvEDvfcr0iuT';
-}
 
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
--- validarAcceso.php
+++ validarAcceso.php
@@ -3,35 +3,26 @@
 //Declaracion de variables
 $mi_rol = $_SESSION['id_tipoCuenta'];
 //----------------------
-
-if (!isset($debug)) {
-    $debug = 0;
+// 1. Proteger la ruta: si no hay sesión activa, regresa al login
+if (!isset($_SESSION['correo_cuenta'])) {
+    header("Location: login.php");
+    exit;
 }
 
-if ($debug == 0)://Si el modo debug esta desactivado, prosigue en la verificacion
-
-// 1. Proteger la ruta: si no hay sesión activa, regresa al login
-    if (!isset($_SESSION['correo_cuenta'])) {
-        header("Location: login.php");
-        exit;
-    }
-
 // 2. Redireccion a pagina adecuada
-    $roles_permitidos_huesped=[1];
-    $roles_permitidos_personal=[2];
-    $roles_permitidos_anfitrion=[2,3];
-    $roles_permitidos_adminitrador=[2,3,4];
-    
-    if (in_array($mi_rol, $roles_permitidos_huesped)) {// Si es Huésped (ID 1), lo mandamos a su propia vista
-        header("Location: ../ModoHuesped/vistaHuesped.php");
-    } else if (in_array($mi_rol, $roles_permitidos_personal)) {// Si es Personal (ID 2), lo mandamos a su propia vista
-        header("Location: ../ModoPersonal/vistaPersonal.php");
-    } else if (in_array($mi_rol, $roles_permitidos_anfitrion)) {// Si es Anfitrion (ID 3), lo mandamos a su propia vista
-        header("Location: ../ModoAnfitrion/vistaAnfitrion.php");
-    } else if (in_array($mi_rol, $roles_permitidos_adminitrador)) {// Si es Administrador (ID 4), lo mandamos a su propia vista
-        header("Location: ../ModoAdministrador/vistaAdministrador.php");
-    } else {// Para cualquier otro rol desconocido
-        header("Location: no_autorizado.php");
-    }
+$roles_permitidos_huesped = [1];
+$roles_permitidos_personal = [2];
+$roles_permitidos_anfitrion = [2, 3];
+$roles_permitidos_adminitrador = [2, 3, 4];
 
-endif;
+if (in_array($mi_rol, $roles_permitidos_huesped)) {// Si es Huésped (ID 1), lo mandamos a su propia vista
+    header("Location: ModoHuesped/vistaHuesped.php");
+} else if (in_array($mi_rol, $roles_permitidos_personal)) {// Si es Personal (ID 2), lo mandamos a su propia vista
+    header("Location: ModoPersonal/vistaPersonal.php");
+} else if (in_array($mi_rol, $roles_permitidos_anfitrion)) {// Si es Anfitrion (ID 3), lo mandamos a su propia vista
+    header("Location: ModoAnfitrion/vistaAnfitrion.php");
+} else if (in_array($mi_rol, $roles_permitidos_adminitrador)) {// Si es Administrador (ID 4), lo mandamos a su propia vista
+    header("Location: ModoAdministrador/vistaAdministrador.php");
+} else {// Para cualquier otro rol desconocido
+    header("Location: no_autorizado.php");
+}
