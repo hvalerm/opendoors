1435b2fa8f5bb0ea9ec4ae56de4796bbf9eb183b
hvalerm <hvalerm@gmail.com>
2026-09-11 19:14:45 -0500

    Nueva version: Mejora en la estructura de archivos Mejora en la modularizacion Mejora en la reutilizacion de codigo Mejora para el modo debug

--- db.php
+++ db.php
@@ -6,16 +6,6 @@ $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
 
-$debug = 1;
-
-if ($debug == 1) {
-    $mi_rol = 0;
-    $host = 'mysql-25019e99-opendoors.j.aivencloud.com';
-    $port = '13195';
-    $dbname = 'opendoors';
-    $user = 'avnadmin';
-    $pass = 'AVNS_9BjfJpsOvEDvfcr0iuT';
-}
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
 
