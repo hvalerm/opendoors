e7fb4593013eaa54db4767a67a0471ce7669373a
hvalerm <hvalerm@gmail.com>
2026-09-11 20:32:32 -0500

    Remove role-based redirection logic

--- validarAcceso.php
+++ validarAcceso.php
@@ -15,14 +15,4 @@ $roles_permitidos_personal = [2];
 $roles_permitidos_anfitrion = [2, 3];
 $roles_permitidos_adminitrador = [2, 3, 4];
 
-if (in_array($mi_rol, $roles_permitidos_huesped)) {// Si es Huésped (ID 1), lo mandamos a su propia vista
-    header("Location: ModoHuesped/vistaHuesped.php");
-} else if (in_array($mi_rol, $roles_permitidos_personal)) {// Si es Personal (ID 2), lo mandamos a su propia vista
-    header("Location: ModoPersonal/vistaPersonal.php");
-} else if (in_array($mi_rol, $roles_permitidos_anfitrion)) {// Si es Anfitrion (ID 3), lo mandamos a su propia vista
-    header("Location: ModoAnfitrion/vistaAnfitrion.php");
-} else if (in_array($mi_rol, $roles_permitidos_adminitrador)) {// Si es Administrador (ID 4), lo mandamos a su propia vista
-    header("Location: ModoAdministrador/vistaAdministrador.php");
-} else {// Para cualquier otro rol desconocido
-    header("Location: no_autorizado.php");
-}
+
